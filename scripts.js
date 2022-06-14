fa4d3c68572382b854cad7083e6877405ed6b298
Sonny Wu <sonnywu@C02GG05FMD6P.tld>
2022-06-14 15:55:27 -0500

    Added snake movement and change of direction functionality

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const BOARD_BORDER_COLOR = 'black'
 const SNAKE_COLOR = 'lightgreen'
 const SNAKE_BORDER_COLOR = 'darkgreen'
 const GAME_BOARD_BG_COLOR = 'black'
+const GAME_REFRESH_SPEED = 150
 
 //Get game board element
 const gameBoard = document.getElementById("gameBoard")
@@ -19,16 +20,25 @@ let snake = [
 
 //Snake velocity
 let dx = 10
-let dy = 10
+let dy = 0
 
 //Begin game
 main()
 
+//Event listender for arrow key change direction
+document.addEventListener('keydown', changeDirection)
+
 //main function
 function main() {
-    createBoard()
-    renderSnake()
-    moveSnake()
+    //Game loop
+    setTimeout(function() {
+        createBoard()
+        renderSnake()
+        moveSnake()
+
+        //Call main again
+        main()
+    }, GAME_REFRESH_SPEED)
 }
 
 //Function to render the gameboard
@@ -69,9 +79,41 @@ function renderSnake() {
 //Function that moves the snake forward by popping the end segment and adding new head to the front
 function moveSnake() {
     //Create new head of snake
-    let head = {x: snake[0].x + dx, y: snake[0].y}
+    let head = {x: snake[0].x + dx, y: snake[0].y + dy}
     //Add new head to the front of the snake
     snake.unshift(head)
     //remove last segment of snake
     snake.pop()
+}
+
+function changeDirection(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = event.keyCode
+
+    //Snake should only go in directions not opposite to it's current direction
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+      }
+      if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+      }
+      if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+      }
+      if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+      }
 }
